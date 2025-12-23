aa7fd65202e232725ee575aa36616f38b705de10
Zhengzihui409 <1983108031@qq.com>
2025-12-23 22:34:24 +0800

    Add files via upload

--- sustc-runner/src/main/java/io/sustc/Application.java
+++ sustc-runner/src/main/java/io/sustc/Application.java
@@ -3,6 +3,7 @@ package io.sustc;
 import io.sustc.benchmark.BenchmarkService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -14,7 +15,10 @@ import org.springframework.shell.ShellApplicationRunner;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        // 强制以 Web (Servlet) 模式启动，避免进入 Spring Shell
+        app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.run(args);
     }
 
     @EventListener
